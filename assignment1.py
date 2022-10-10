a876b26f0fef262a834e6c7ddfe14e5f02d0c516
Mahesh <venkatmahesh2001@gmail.com>
2022-10-10 09:02:34 +0530

    assignments

---
+++ assignment1.py
@@ -0,0 +1,32 @@
+#1.what is variable ? write varibles rules
+# variable.
+# variable is named memory location which is useful to store the data
+# syntax: var_name=value
+# rules:
+# variable name must start with alphabets(lower to upper)or with underscore but no special
+# characters are not allowed
+# variable name cannot start with a number
+# variable name can have alphanum characters and underscores
+# variable names are case senstive
+# variable name should not be a keyword
+
+#2.what is date type? how many data types in python. write some examples?
+# data type:
+# data type represents the type of the data present inside the variable
+# we no need to specify the type explicity to the variable
+# data types are int,float,complex,bool,string,bytes,bytes array,range,list,set,tuple,forzenset,
+# dictionary,none
+# bytes,bytearray,complex,frozenset,are rarely used data types
+
+# int: we can use int data type to represent the whole numbers
+# x=12
+# print(x(type(x)))
+# output:12<class'int'>
+
+
+
+
+
+
+
+
